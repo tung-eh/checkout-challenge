be397ec02d1aaac4331488538b022c8618766a0d
Tung Teng <thetungctn@gmail.com>
2021-01-18 04:26:26 +0700

    Input component

--- src/CheckoutForm.js
+++ src/CheckoutForm.js
@@ -4,18 +4,40 @@ import Input from './Input'
 
 const CheckoutForm = () => {
   return (
-    <div className="grid grid-cols-form py-14">
+    <div className="grid grid-cols-form gap-32 py-14 mb-20">
       <div>
-        <h2 className="font-bold text-lg text-gray-700">Contact information</h2>
-        <Input placeholder="Enter your email.." />
-        <Input placeholder="Enter your phone.." />
-        <h2 className="font-bold text-lg text-gray-700">Shipping address</h2>
-        <Input placeholder="Your name.." />
-        <Input placeholder="Your address.." />
-        <Input placeholder="Your city.." />
-        <div className="grid grid-cols-2">
-          <Input placeholder="Your country.." />
-          <Input placeholder="Your postal code.." />
+        <h2 className="font-bold text-lg text-gray-700 mb-3">
+          Contact information
+        </h2>
+        <Input
+          label="E-mail"
+          icon="email"
+          type="email"
+          placeholder="Enter your email.."
+        />
+        <Input
+          label="Phone"
+          icon="phone"
+          type="tel"
+          placeholder="Enter your phone.."
+        />
+        <h2 className="font-bold text-lg text-gray-700 mt-16 mb-3">
+          Shipping address
+        </h2>
+        <Input
+          label="Full name"
+          icon="account_circle"
+          placeholder="Your name.."
+        />
+        <Input label="Address" icon="home" placeholder="Your address.." />
+        <Input label="City" icon="location_city" placeholder="Your city.." />
+        <div className="grid grid-cols-2 gap-8">
+          <Input label="Country" icon="public" placeholder="Your country.." />
+          <Input
+            label="Postal code"
+            icon="markunread_mailbox"
+            placeholder="Your postal code.."
+          />
         </div>
         <input type="checkbox" />
         Save this information for next time
--- src/Input.js
+++ src/Input.js
@@ -1,7 +1,35 @@
-const Input = (props) => (
-  <div>
-    <input {...props} />
-  </div>
-)
+import React from 'react'
+
+import Icon from './Icon'
+
+// Copy from https://gist.github.com/gordonbrander/2230317
+const generateId = () => '_' + Math.random().toString(36).substr(2, 9)
+
+const Input = ({ label, icon, id, ...props }) => {
+  const inputId = id || generateId()
+
+  return (
+    <div className="relative flex flex-col mb-4">
+      {label && (
+        <label
+          className="font-bold text-xs text-gray-500 mb-1"
+          htmlFor={inputId}
+        >
+          {label}
+        </label>
+      )}
+      <input
+        className="border border-gray-500 placeholder-gray-800 rounded-xl outline-none font-bold p-4 pl-14"
+        id={inputId}
+        {...props}
+      />
+      <Icon
+        className="absolute bottom-4 left-4 fill-current text-gray-500 pointer-events-none"
+        name={icon}
+        style={{ marginBottom: '1px' }} //input border
+      />
+    </div>
+  )
+}
 
 export default Input
